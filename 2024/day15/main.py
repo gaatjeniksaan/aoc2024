5841942da70a4ca472dc60bb6a08176c93fa5fd9
gaatjeniksaan <barendlinders@gmail.com>
2024-12-19 12:39:42 +0100

    day15 pt2

--- 2024/day15/main.py
+++ 2024/day15/main.py
@@ -1,5 +1,5 @@
+import collections
 from pathlib import Path
-from shutil import move
 import time
 from typing import Final
 
@@ -24,8 +24,8 @@ def main():
     # print(f"Execution time for part1: {elapsed_time_ms:.2f} ms")
     # aocd.submit(solution1, part="a", day=DAY, year=YEAR)
 
-    answer2 = part2(sample_input=True) == EXAMPLE_ANSWER_2
-    assert answer2 == EXAMPLE_ANSWER_2, f"calculated answer '{answer2}' != expected answer '{EXAMPLE_ANSWER_2}'"
+    # answer2 = part2(sample_input=True) == EXAMPLE_ANSWER_2
+    # assert answer2 == EXAMPLE_ANSWER_2, f"calculated answer '{answer2}' != expected answer '{EXAMPLE_ANSWER_2}'"
     start_time = time.time()
     solution2 = part2()
     end_time = time.time()
@@ -71,34 +71,40 @@ def update_grid(
 
 def update_grid_2(
     grid: dict[tuple[int, int], str],
-    start: tuple[int, int], 
-    direction: tuple[int, int], 
-    items: list[str],
-) -> tuple[int, int]:
-    priority = {".": 0, "@": 1}
-    if direction == (1, 0):
-        # We are moving right
-        priority["["] = 3
-        
-    sorted_items = sorted(items, key=lambda item: priority[item])
-    reply: tuple[int, int] = (0, 0)
-    # print(f"items to move: {items}")
-    for n, char in enumerate(sorted_items):
-        current = (start[0] + n * direction[0], start[1] + n * direction[1])
-        # print(f"current: {current}")
-        grid[current] = char
-        if char == "@":
-            print(f"@ goes to {current}")
-            reply = current
-
-    # print("\n")
-    return reply
+    grid_update: list[tuple[int, int]], 
+    direction: tuple[int, int],
+) -> None:
+    if direction == (1, 0):  # East -> max-x first
+        grid_update = sorted(grid_update, key=lambda x: x[0], reverse=True)
+    if direction == (-1, 0):  # West -> min-x first
+        grid_update = sorted(grid_update, key=lambda x: x[0], reverse=False)
+    if direction == (0, 1):  # South -> max-y first
+        grid_update = sorted(grid_update, key=lambda x: x[1], reverse=True)
+    if direction == (0, -1):  # North -> min-y first
+        grid_update = sorted(grid_update, key=lambda x: x[1], reverse=False)
+    
+    print(f"{direction=}:{grid_update=}")
+    for coord in grid_update:
+        char = grid[coord]
+        grid[coord] = "."
+        grid[(coord[0] + direction[0], coord[1] + direction[1])] = char
 
 
 def calculate_gps(grid: dict[tuple[int, int], str]) -> int:
     total = 0
     for (x, y), char in grid.items():
         if char != "O":
+            continue
+
+        total += (x + 100 * y)
+
+    return total
+
+
+def calculate_gps_2(grid: dict[tuple[int, int], str]) -> int:
+    total = 0
+    for (x, y), char in grid.items():
+        if char != "[":
             continue
 
         total += (x + 100 * y)
@@ -162,10 +168,13 @@ def part2(sample_input: bool = False) -> str:
     grid: dict[tuple[int, int], str] = {}
     current: tuple[int, int] = (0, 0)
     for y, line in enumerate(grid_data.split("\n")):
+        l = ""
         for x, char in enumerate(line):
+            l += char
             grid[(x, y)] = char
             if char == "@":
                 current = (x, y)
+        print(l)
 
     direction_mapper: dict[str, tuple[int, int]] = {
         "^": (0, -1),
@@ -174,36 +183,77 @@ def part2(sample_input: bool = False) -> str:
         "<": (-1, 0),
     }
 
-    # print(f"grid before: {grid}\n\n")
     for i, move in enumerate(movements, start=1):
         print(f"{move=} for {i=}")
         direction = direction_mapper[move]
-        n = 1
-        items: list[str] = ["@"]
 
-        while True:
-            next_char = look_ahead(grid, current, direction, n)
+        # Collect a list of all items that want to be moved
+        grid_update: list[tuple[int, int]] = []
+        to_check_queue = collections.deque([current])
+        should_update = True
+
+        while to_check_queue:
+            # Grab the current location to check
+            coord = to_check_queue.pop()
+            grid_update.append(coord)
+            current_char = grid[coord]
+
+            # Next coordinate to inspect
+            next_coord = (coord[0] + direction[0], coord[1] + direction[1])
+            next_char = grid[next_coord]
+
+            # If we hit a "#" we are doneski
             if next_char == "#":
-                break
-
-            if next_char == ".":
-                items.append(".")
+                should_update = False
                 break
             
-            items.append(next_char)
-            n += 1
+            # If we hit a "." this path is free to move, nothing to do
+            if next_char == ".":
+                continue
+            
+            # When moving left to right we can progress normally
+            if move in [">", "<"]:
+                if next_char not in ["[", "]"]:
+                    raise ValueError(f"weird state: {next_char=}:{direction=}")
+                to_check_queue.append(next_coord)
+            if move in ["^", "v"]:
+                to_check_queue.append(next_coord)
+                if next_char == "]":
+                    # We need to add char to the left
+                    neighbour_coord = (next_coord[0] - 1, next_coord[1])
+
+                elif next_char == "[":
+                    # We need to add char to the right
+                    neighbour_coord = (next_coord[0] + 1, next_coord[1])
+                else:
+                    raise ValueError(f"weird state: {next_char=}:{direction=}")
+
+                to_check_queue.append(neighbour_coord)
         
-        # Update grid and next iteration
-        current = update_grid_2(grid, current, direction, items)
-    
-        # print(f"grid after {i=}: \n{grid}\n\n")
-    
-    answer = calculate_gps(grid)
-    
+        if should_update:
+            # Don't forget to add our robot's position
+            # grid_update[current] = "@"
+            print(f"updating grid with: {grid_update=}")
+            update_grid_2(grid, list(set(grid_update)), direction)
+            
+            # Update our robot
+            current = (current[0] + direction[0], current[1] + direction[1])
+            print_grid(grid)
+
+    # print_grid(grid)
+    answer = calculate_gps_2(grid)
+    raise ValueError(answer)
     # Your implementation goes here
-    answer = "geenidee"
-    return answer
+    return str(answer)
  
+
+def print_grid(grid):
+    for y in range(10):
+        l = ""
+        for x in range(20):
+            l += grid[(x, y)]
+        print(l)
+
 
 def load_data(sample_input: bool) -> str:
     p = Path(__file__).parent.absolute()
